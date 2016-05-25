618197d8f83944d97c084c9fe191612b12ee8f87
Thomas Toye <toye.thomas@gmail.com>
2016-05-25 22:03:54 +0200

    Add child list and child service

--- app/pages/page2/page2.ts
+++ app/pages/page2/page2.ts
@@ -1,11 +1,31 @@
 import {Page} from 'ionic-angular';
+import {ChildService} from "../../providers/child-service/child-service";
 
 
 @Page({
   templateUrl: 'build/pages/page2/page2.html',
+  providers: [ChildService],
 })
 export class Page2 {
-  constructor() {
+  children: any[] = [];
+  searchQuery: string = '';
+  _childService: ChildService;
 
+  constructor(_childService: ChildService) {
+    this._childService = _childService;
+    this.getItems();
   }
+
+  getItems() {
+    var startTime = new Date();
+
+    this._childService
+      .findByName(this.searchQuery)
+      .then(res => {
+        console.log(`took ${new Date().getTime() - startTime.getTime()}ms to get and filter children`);
+        this.children = res;
+      })
+      .catch(e => console.error(e));
+  }
+
 }
---
+++ app/providers/child-service/child-service.ts
@@ -0,0 +1,55 @@
+import {Injectable} from '@angular/core';
+
+declare var PouchDB:any;
+
+@Injectable()
+export class ChildService {
+  db:any;
+  remote:string = 'http://localhost:5984/test-children';
+  data:Array<any> = [];
+
+
+  constructor() {
+    this.db = new PouchDB('some-db');
+
+    let options = {
+      live: true,
+      retry: true,
+      continuous: true,
+      // auth: { username: this.username, password: this.password }
+    };
+
+    this.db.sync(this.remote, options);
+  }
+
+  getAll():Promise<Array<any>> {
+    return this.db
+      .query('child/all')
+      .then(result => result.rows.map(row => row.value))
+  }
+
+  findByName(name:string, limit:number = 25):Promise<any> {
+    name = name || '';
+
+    return this
+      .getAll()
+      .then((res) => {
+        return res.filter((row) => {
+          return (row.firstName + ' ' + row.lastName).toLowerCase().indexOf(name.toLowerCase()) != -1
+        })
+      });
+  }
+
+  deleteChild(id:string) {
+    this.db.get(id).then((obj) => {
+      obj.id = 'type/child/v1/deleted';
+      this.db.put(obj);
+    })
+  }
+
+  insertChild(child: any):Promise<any> {
+    this.db.put(child);
+  }
+
+}
+
